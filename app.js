4dcf35253713c5394ff17b824e60957c5571f1c9
Thomas Hai Li <thomas.hai.li98@gmail.com>
2019-06-29 03:08:25 -0400

    fix: correct git repo name

--- app.js
+++ app.js
@@ -58,4 +58,4 @@ app.post("/feedback", (req, res) => {
     });
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+// app.listen(port, () => console.log(`App served on localhost:${port}`));
